2847a2e4d3d015a1f326ddd88954aa3eae602c72
duidae <duidae@gmail.com>
2024-05-03 00:40:12 +0800

    fix: fix buid err

--- src/app/component/map.tsx
+++ src/app/component/map.tsx
@@ -2,13 +2,61 @@
 import {useState, useRef, useMemo} from "react"
 import L, {LatLngExpression} from "leaflet"
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
-import {TAIWAN_CENTER, DEFAULT_ZOOM} from "@/app/constant"
+import {TAIWAN_CENTER, DEFAULT_ZOOM, Sculpture} from "@/app/constant"
 
 import {Sculptures} from "@/app/mockup"
 
 import MarkerIcon from "leaflet/dist/images/marker-icon.png"
 import MarkerShadow from "leaflet/dist/images/marker-shadow.png"
 import "leaflet/dist/leaflet.css"
+
+const MarkerComponent = (props: {sculpture: Sculpture}) => {
+  const {sculpture} = props
+  const markerRef = useRef<L.Marker>(null)
+
+  const eventHandlers = useMemo(
+    () => ({
+      mouseover() {
+        markerRef?.current?.openPopup()
+      }
+    }),
+    []
+  )
+
+  const markerIcon = new L.Icon({
+    iconUrl: MarkerIcon.src,
+    iconRetinaUrl: MarkerIcon.src,
+    iconSize: [25, 41],
+    iconAnchor: [12.5, 41],
+    popupAnchor: [0, -41],
+    shadowUrl: MarkerShadow.src,
+    shadowSize: [41, 41]
+  })
+
+  const sculptureInfo = (
+    <>
+      <span className="text-lg font-bold">
+        {sculpture.title} - {sculpture.artist}
+      </span>
+      <br />
+      <span className="text-lg">{sculpture.location}</span>
+      <br />
+      <span className="text-base">{sculpture.desc}</span>
+      <br />
+      {sculpture.link && (
+        <a className="text-lg" href={sculpture.link}>
+          連結
+        </a>
+      )}
+    </>
+  )
+
+  return (
+    <Marker ref={markerRef} icon={markerIcon} eventHandlers={eventHandlers} position={sculpture.lnglat as LatLngExpression}>
+      <Popup>{sculptureInfo}</Popup>
+    </Marker>
+  )
+}
 
 const Map = () => {
   const [coord, setCoord] = useState<LatLngExpression>(TAIWAN_CENTER as LatLngExpression)
@@ -36,56 +84,7 @@ const Map = () => {
 
   // TODO: fetch link og
   const sculptureMarkers = Sculptures.map((sculpture, cIndex) => {
-    const markerRef = useRef<L.Marker>(null)
-
-    const eventHandlers = useMemo(
-      () => ({
-        mouseover() {
-          markerRef?.current?.openPopup()
-        }
-      }),
-      []
-    )
-
-    const markerIcon = new L.Icon({
-      iconUrl: MarkerIcon.src,
-      iconRetinaUrl: MarkerIcon.src,
-      iconSize: [25, 41],
-      iconAnchor: [12.5, 41],
-      popupAnchor: [0, -41],
-      shadowUrl: MarkerShadow.src,
-      shadowSize: [41, 41]
-    })
-
-    const sculptureInfo = (
-      <>
-        <span className="text-lg font-bold">
-          {sculpture.title} - {sculpture.artist}
-        </span>
-        <br />
-        <span className="text-lg">{sculpture.location}</span>
-        <br />
-        <span className="text-base">{sculpture.desc}</span>
-        <br />
-        {sculpture.link && (
-          <a className="text-lg" href={sculpture.link}>
-            連結
-          </a>
-        )}
-      </>
-    )
-
-    return (
-      <Marker
-        ref={markerRef}
-        key={`marker-${cIndex}`}
-        icon={markerIcon}
-        eventHandlers={eventHandlers}
-        position={sculpture.lnglat as LatLngExpression}
-      >
-        <Popup>{sculptureInfo}</Popup>
-      </Marker>
-    )
+    return <MarkerComponent key={`marker-${cIndex}`} sculpture={sculpture} />
   })
 
   return (
